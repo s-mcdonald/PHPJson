ddb38ec3d246d9bdcbd6cce3b81996fddfaafc39
SamM <s.mcdonald@outlook.com.au>
2025-05-31 11:30:39 +1000

    feat: add JsonBuilderException

---
+++ src/Builder/Exceptions/JsonBuilderException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SamMcDonald\Json\Builder\Exceptions;
+
+use RuntimeException;
+
+final class JsonBuilderException extends RuntimeException
+{
+    public static function createWithMessage(string $message): self
+    {
+        return new self($message);
+    }
+}
