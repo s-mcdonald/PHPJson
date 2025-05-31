b0044e9a43d8fbb3a499fa6da0e25a0fdfbb43cb
SamM <s.mcdonald@outlook.com.au>
2025-05-31 11:45:10 +1000

    refactor: remove the AbstractJsonBuilder.php
    * Move logic into the builder itself

--- src/Builder/JsonBuilder.php
+++ src/Builder/JsonBuilder.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Builder;
 
+use InvalidArgumentException;
+use SamMcDonald\Json\Builder\Exceptions\JsonBuilderException;
 use SamMcDonald\Json\Json;
+use SamMcDonald\Json\Serializer\Encoding\Components\JsonToStdClassDecoder;
 use SamMcDonald\Json\Serializer\Exceptions\JsonException;
+use stdClass;
+use Throwable;
 
-final class JsonBuilder extends AbstractJsonBuilder
+final class JsonBuilder
 {
+    private array $jsonProperties = [];
+
+    public function __toString(): string
+    {
+        try {
+            return json_encode($this->jsonProperties, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+        } catch (Throwable $t) {
+            throw JsonBuilderException::createWithMessage($t->getMessage());
+        }
+    }
+
     public function addNullProperty(string $prop): self
     {
         self::assertPropertyName($prop);
@@ -28,5 +44,95 @@ final class JsonBuilder extends AbstractJsonBuilder
         }
 
         return $builder;
+    }
+
+    public function addProperty(string $prop, mixed $value): self
+    {
+        self::assertPropertyName($prop);
+
+        return $this->addProp($prop, self::cleanValue($value, $this));
+    }
+
+    public function toStdClass(): stdClass
+    {
+        try {
+            return (new JsonToStdClassDecoder())->decode((string) $this)->getBody();
+        } catch (Throwable $t) {
+            throw JsonBuilderException::createWithMessage($t->getMessage());
+        }
+    }
+
+    public function toArray(): array
+    {
+        try {
+            return json_decode((string) $this, true, 512, JSON_THROW_ON_ERROR);
+        } catch (Throwable $t) {
+            throw JsonBuilderException::createWithMessage($t->getMessage());
+        }
+    }
+
+    public function build(): string
+    {
+        return (string) $this;
+    }
+
+    public function removeProperty(string $prop): self
+    {
+        unset($this->jsonProperties[$prop]);
+
+        return $this;
+    }
+
+    protected static function assertPropertyName(string $prop): void
+    {
+        if (!preg_match('/^[a-zA-Z]{1}[a-zA-Z0-9-_]*$/', $prop)) {
+            throw new InvalidArgumentException('Invalid property name');
+        }
+    }
+
+    protected static function cleanValue(mixed $value, self $instance): mixed
+    {
+        if ($value instanceof self) {
+            return $value->toStdClass();
+        }
+
+        if (is_array($value)) {
+            $value = self::cleanArrayValue($value, $instance);
+        }
+
+        if (is_object($value)) {
+            throw new InvalidArgumentException('Does not support Object types.');
+        }
+
+        return match (gettype($value)) {
+            'array', 'string', 'double', 'integer', 'boolean', 'NULL' => $value,
+            default => throw new InvalidArgumentException('Invalid value type - Received : ' . gettype($value)),
+        };
+    }
+
+    protected static function cleanArrayValue(array $value, self $instance): array
+    {
+        $returnArray = [];
+
+        foreach ($value as $key => $val) {
+            if ($val === $instance) {
+                throw new InvalidArgumentException('Recursion error: Does not support itself');
+            }
+            if (is_array($val)) {
+                $returnArray[$key] = self::cleanArrayValue($val, $instance);
+                continue;
+            }
+
+            $returnArray[$key] = self::cleanValue($val, $instance);
+        }
+
+        return $returnArray;
+    }
+
+    private function addProp(string $prop, mixed $value = null): self
+    {
+        $this->jsonProperties[$prop] = $value;
+
+        return $this;
     }
 }
