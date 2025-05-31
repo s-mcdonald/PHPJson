c9e6b3fc60417bca2c64cc585b379e92ca3f4255
SamM <s.mcdonald@outlook.com.au>
2025-05-31 15:26:40 +1000

    deprecate JsonFormatter.php

--- src/Serializer/Formatter/JsonFormatter.php
+++ src/Serializer/Formatter/JsonFormatter.php
@@ -4,29 +4,18 @@ declare(strict_types=1);
 
 namespace SamMcDonald\Json\Serializer\Formatter;
 
+/**
+ * @deprecated
+ */
 final class JsonFormatter
 {
     public function pretty(string $json): string
     {
-        return $this->reEncode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        return json_encode(json_decode($json, false), JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
     }
 
     public function ugly(string $json): string
     {
-        return $this->reEncode($json, JSON_UNESCAPED_SLASHES);
-    }
-
-    private function reEncode(string $json, int $options): string
-    {
-        $decoded = json_decode($json, true, 512);
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            return $json;
-        }
-        $encoded = json_encode($decoded, $options);
-        if (false === $encoded) {
-            return $json;
-        }
-
-        return $encoded;
+        return json_encode(json_decode($json, false), JSON_THROW_ON_ERROR);
     }
 }
