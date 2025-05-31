0631e746df8ef4b1af9a2fe131a629e5f807f2a8
SamM <s.mcdonald@outlook.com.au>
2025-05-31 12:00:24 +1000

    feat: added JsonSerializerInterface.php

---
+++ src/Serializer/JsonSerializerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SamMcDonald\Json\Serializer;
+
+use SamMcDonald\Json\Serializer\Enums\JsonFormat;
+
+interface JsonSerializerInterface
+{
+    public function serialize(mixed $object, JsonFormat $format = JsonFormat::Compressed): string;
+
+    public function deserialize(string $json, string $classFqn): mixed;
+}
